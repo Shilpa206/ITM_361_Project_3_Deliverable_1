0dc153e1af108d7f0f5d5b17f4e73ad9d2b90458
Shilpa206 <111149272+Shilpa206@users.noreply.github.com>
2022-11-22 21:30:04 -0600

    working on java script

--- js/index.js
+++ js/index.js
@@ -1,30 +1,17 @@
 function initMap() {
-	const myLatLng = { lat:48.8566, lng:2.3522};
-	const map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 3,
-		center: myLatLng,
-	});
-
-	new google.maps.Marker({
-    	position: myLatLng,
-    	map,
- 	 });
-
-
-	new google.maps.Marker({
-        position: new google.maps.LatLng(45.47859954420433, 9.227346521619015),
-        map,
-        label: "Milan, Italy",
-        animation: google.maps.Animation.DROP
-	});
-	
-	new google.maps.Marker({
-        position: new google.maps.LatLng(51.5072,0.1276),
-        map,
-        label: "London, England",
-        animation: google.maps.Animation.DROP
-	});
-
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: { lat: 41.893516609387966,lng: 87.6297536812628}
+  });
+  
+  var marker = new google.maps.Marker({
+    position: { lat: 41.893516609387966, lng: 87.6297536812628 },
+    map: map,
+  });
+  
+  var infowindow = new google.maps.InfoWindow({
+    content:"Tao, Chicago"
+  });
+  infowindow.open(map, marker);
 }
-
 window.initMap = initMap;
