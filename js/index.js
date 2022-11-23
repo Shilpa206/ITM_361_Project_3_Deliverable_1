9ffef3dce6df732ade8f7071dad17cc8c227ebe0
Shilpa206 <111149272+Shilpa206@users.noreply.github.com>
2022-11-22 19:49:46 -0600

    added java script code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 function initMap() {
+	const myLatLng = { lat:48.8566, lng:2.3522};
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 4,
+		center: myLatLng,
+	});
+
+	new google.maps.Marker({
+    	position: myLatLng,
+    	map,
+ 	 });
+
+
+	new google.maps.Marker({
+        position: new google.maps.LatLng(45.47859954420433, 9.227346521619015),
+        map,
+        label: "Milan, Italy",
+        animation: google.maps.Animation.DROP
+	});
+	
+	new google.maps.Marker({
+        position: new google.maps.LatLng(51.5072,0.1276),
+        map,
+        label: "London, England",
+        animation: google.maps.Animation.DROP
+	});
+
+}
+
+window.initMap = initMap;
